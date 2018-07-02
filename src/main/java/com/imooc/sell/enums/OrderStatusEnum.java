0ab59e823a78b756628047cb7eb44e62c6bdc083
Robert <346654970@qq.com>
2018-07-02 11:19:21 +0800

    1

--- src/main/java/com/imooc/sell/enums/OrderStatusEnum.java
+++ src/main/java/com/imooc/sell/enums/OrderStatusEnum.java
@@ -9,9 +9,9 @@ import lombok.Getter;
 @Getter
 public enum  OrderStatusEnum  implements CodeEnum{
     NEW(0,"新订单"),
-        FINISH(1,"完结"),
-            CANCEL(2,"已取消"),
-                ;
+    FINISH(1,"完结"),
+    CANCEL(2,"已取消"),
+    ;
 
     private  Integer code;
     private  String message;

34ad5ade0cf3123d5ac94eea61e91bb255de8a6c
Robert <346654970@qq.com>
2018-07-04 11:32:36 +0800

    1

---
+++ src/main/java/com/imooc/sell/handler/SellExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.imooc.sell.handler;
+
+
+import com.imooc.sell.config.ProjectUrlConfig;
+import com.imooc.sell.exception.SellerAuthorizeException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by 廖师兄
+ * 2017-07-30 17:44
+ */
+//《@ControllerAdvice + @ExceptionHandler 全局处理 Controller 层异常》 word文档
+@ControllerAdvice
+public class SellExceptionHandler {
+
+    @Autowired
+    private ProjectUrlConfig projectUrlConfig;
+
+    //拦截登录异常
+    //http://sell.natapp4.cc/sell/wechat/qrAuthorize?returnUrl=http://sell.natapp4.cc/sell/seller/login
+    @ExceptionHandler(value = SellerAuthorizeException.class)
+    public ModelAndView handlerAuthorizeException() {
+        //concat只能接字符串。
+        //+可以接其它类型。
+        return new ModelAndView("redirect:"
+        .concat(projectUrlConfig.getWechatOpenAuthorize())
+        .concat("/sell/wechat/qrAuthorize")
+        .concat("?returnUrl=")
+        .concat(projectUrlConfig.getSell())
+        .concat("/sell/seller/login"));
+    }
+}
